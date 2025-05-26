7d183dc089702e3d8ca5af906c15800885cf3afe
Joshua <queonda34@live.com.mx>
2025-05-26 11:34:46 -0600

    Revert "Borrado de comentarios extra"
    This reverts commit 79beea950beb2c97f8ae7dfb78cda7ad35aece65.

--- api/index.php
+++ api/index.php
@@ -1,11 +1,11 @@
 <?php
-
+// api/index.php
 $request_uri = $_SERVER['REQUEST_URI'];
 $request_method = $_SERVER['REQUEST_METHOD'];
 $path = trim(str_replace('/api/cinepolis', '', $request_uri), '/');
 $segments = explode('/', $path);
 
-header('Content-Type: application/xml'); 
+header('Content-Type: application/xml'); // Establecer el tipo de contenido a XML
 
 switch ($segments[0]) {
     case 'peliculas':
@@ -16,7 +16,7 @@ switch ($segments[0]) {
         break;
     default:
         header("HTTP/1.1 404 Not Found");
-        
+        // Si también quieres devolver un cuerpo XML para el error 404:
         echo '<?xml version="1.0" encoding="UTF-8"?>';
         echo '<error>';
         echo '<message>Ruta no encontrada</message>';
